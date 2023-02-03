a43e554a2ad7ac6cc205a0e4c813ce9288c853bd
CemAkan <cemakan5014@gmail.com>
2023-02-03 15:23:34 +0300

    response edited

--- routes/createRoute.js
+++ routes/createRoute.js
@@ -1,23 +1,9 @@
-const { error } = require("console");
 var express = require("express");
 var router = express.Router();
-const fs = require("fs");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  fs.readFile(
-    "/home/cem/Documents/codes/resignation_webpage/views/create.html",
-    function (error, data) {
-      res.setHeader("Content-Type", "text/html");
-      if (error) {
-        res.writeHead(404);
-        res.write("Error occured");
-      } else {
-        res.write(data);
-      }
-      res.end();
-    }
-  );
+  res.render("create");
 });
 
 module.exports = router;
