0a62a2ee6213dfeecfa8998b12840fd6ad8991fa
CemAkan <cemakan5014@gmail.com>
2023-02-04 12:59:27 +0300

    id bug fixed

--- public/javascripts/delete.js
+++ public/javascripts/delete.js
@@ -1,5 +1,5 @@
 const ID = document.getElementById("ID");
-const button = document.getElementById("listBtn");
+const button = document.getElementById("deleteBtn");
 
 button.addEventListener("click", () => {
   fetch("http://localhost:2000/destroy/?id=" + ID.value).then(
