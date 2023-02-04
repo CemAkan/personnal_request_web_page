11dabf5fd67ce4cc0a564f0b93f5212d6076cd38
CemAkan <cemakan5014@gmail.com>
2023-02-04 13:17:34 +0300

    .catch() added

--- public/javascripts/updating.js
+++ public/javascripts/updating.js
@@ -9,31 +9,33 @@ var personUpdate = (ID, username, email, phone, active) => {
       where: {
         id: ID,
       },
-    }).then((person) => {
-      if (person) {
-        if (phone.length == 0) {
-          phone = person.phone;
-        }
-        if (active.length == 0) {
-          active = person.active;
-        }
-        if (username.length == 0) {
-          username = person.username;
-        }
-        if (email.length == 0) {
-          email = person.email;
-        }
+    })
+      .then((person) => {
+        if (person) {
+          if (phone.length == 0) {
+            phone = person.phone;
+          }
+          if (active.length == 0) {
+            active = person.active;
+          }
+          if (username.length == 0) {
+            username = person.username;
+          }
+          if (email.length == 0) {
+            email = person.email;
+          }
 
-        person
-          .update({
-            username: username,
-            email: email,
-            phone: phone,
-            active: active,
-          })
-          .then((user) => console.log(user.dataValues));
-      }
-    });
+          person
+            .update({
+              username: username,
+              email: email,
+              phone: phone,
+              active: active,
+            })
+            .then((user) => console.log(user.dataValues));
+        }
+      })
+      .catch((error) => console.log("Person can not found !!!"));
   });
 };
 
